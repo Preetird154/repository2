6f1093e5f32072559e8fc40645b8bb9891411cb4
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-10 12:49:27 +0530

    Multiplication program

---
+++ hello/src/example/Multiplication.java
@@ -0,0 +1,11 @@
+package example;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int a=10, b=2;
+		int res=a*b;
+		System.out.println("Multiplication: "+res);
+	}
+
+}
