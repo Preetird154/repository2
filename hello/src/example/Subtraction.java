baa3f36274286f13a3cb5fed725581356005494a
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-10 12:33:57 +0530

    updated subtraction program
    added one more line

--- hello/src/example/Subtraction.java
+++ hello/src/example/Subtraction.java
@@ -7,6 +7,8 @@ public class Subtraction {
 		System.out.println("subtraction of "+a+ "and "+b+" is "+(a-b));
 		
 		System.out.println("Added new line");
+		
+		System.out.println("Added one more line");
 	}
 
 }
