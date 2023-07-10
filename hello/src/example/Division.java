6c9611ec52364f6818775362c12a67b0ffeac785
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-10 14:27:30 +0530

    added one method in division program

--- hello/src/example/Division.java
+++ hello/src/example/Division.java
@@ -2,29 +2,33 @@ package example;
 
 class abc{
 	
+	int a=10;
+	int b=2;
+	
 	public void div()
 	{
-		int a=10;
-		int b=2;
 		int res=a/b;
 		System.out.println("Division: "+res);
 	}
 	
 	public void mul()
 	{
-		int a=10;
-		int b=2;
 		int res=a*b;
 		System.out.println("Multiplication: "+res);
 	}
+	
+	public void add()
+	{
+		int res=a+b;
+		System.out.println("Addition: "+res);
+	}
 }
-
 
 public class Division {
 	public static void main(String[] args) {
 		abc a=new abc();
 		a.div();
 		a.mul();
+		a.add();
 	}
-
 }
