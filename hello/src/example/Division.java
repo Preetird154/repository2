5d4150c534927e75b99babfd789b506ab790627d
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-10 12:56:59 +0530

    updated division program
    added new method in division program

--- hello/src/example/Division.java
+++ hello/src/example/Division.java
@@ -1,12 +1,21 @@
 package example;
 
 class abc{
+	
 	public void div()
 	{
 		int a=10;
 		int b=2;
 		int res=a/b;
 		System.out.println("Division: "+res);
+	}
+	
+	public void mul()
+	{
+		int a=10;
+		int b=2;
+		int res=a*b;
+		System.out.println("Multiplication: "+res);
 	}
 }
 
@@ -15,6 +24,7 @@ public class Division {
 	public static void main(String[] args) {
 		abc a=new abc();
 		a.div();
+		a.mul();
 	}
 
 }
