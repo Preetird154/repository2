3ef86a4bae7ec63f5ac90642dc9861973c7dc2db
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-10 12:32:33 +0530

    added new line

---
+++ hello/src/example/Subtraction.java
@@ -0,0 +1,12 @@
+package example;
+
+public class Subtraction {
+	
+	public static void main(String[] args) {
+		int a=30, b=20;
+		System.out.println("subtraction of "+a+ "and "+b+" is "+(a-b));
+		
+		System.out.println("Added new line");
+	}
+
+}
