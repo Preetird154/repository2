a8cee1c444db006103b07b7c03855b17876e1aa3
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-11 18:28:15 +0530

    commit

--- hello/src/Pakage3/class2.java
+++ hello/src/Pakage3/class2.java
@@ -4,6 +4,8 @@ public class class2 {
 	public static void main(String[] args) {
 		System.out.println("swathi");
 		System.out.println("EFGH");
+		int preeti=10;
+		System.out.println("preeti");
 	}
 
 }
