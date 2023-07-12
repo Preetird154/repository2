84cb9d14a0682e5e26a4182de2c9abd34fb81200
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-12 14:46:44 +0530

    commit

--- hello/src/Practice/DEF.java
+++ hello/src/Practice/DEF.java
@@ -2,9 +2,9 @@ package Practice;
 
 public class DEF {
 	public static void main(String[] args) {
-		int x=89;
-		int y=76;
-		System.out.println(x+y);
+		int x1=89;
+		int y1=76;
+		System.out.println(x1+y1);
 	}
 
 }
