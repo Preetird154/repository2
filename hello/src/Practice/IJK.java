dca62579ae478674305162dbafc089a38f1c945f
Preetird154 <138884787+Preetird154@users.noreply.github.com>
2023-07-12 15:43:49 +0530

    commit

--- hello/src/Practice/IJK.java
+++ hello/src/Practice/IJK.java
@@ -2,9 +2,9 @@ package Practice;
 
 public class IJK {
 	public static void main(String[] args) {
-		int a2=28;
-		int b2=89;
-		System.out.println(a2+b2);
+		int a5=28;
+		int b5=89;
+		System.out.println(a5+b5);
 	}
 
 }
